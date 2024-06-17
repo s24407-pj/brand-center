43db1aab403f98dee64d45783ec644938a984a59
Michal <s24407@pjwstk.edu.pl>
2024-06-17 21:34:46 +0200

    fix instagram data fetch

--- src/app/api/instagram/route.ts
+++ src/app/api/instagram/route.ts
@@ -2,9 +2,10 @@
 import {NextResponse} from 'next/server'
 
 export async function GET() {
+	const myId = '25518565757791266'
 	try {
 		const response = await fetch(
-			`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${process.env.IG_TOKEN}`,
+			`https://graph.instagram.com/${myId}/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${process.env.IG_TOKEN}`,
 		)
 
 		if (!response.ok) {
@@ -12,6 +13,7 @@ export async function GET() {
 		}
 
 		const data = await response.json()
+		console.log(data)
 		return NextResponse.json(data)
 	} catch (error: any) {
 		return NextResponse.json({error: error.message}, {status: 500})
